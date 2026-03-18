f230a2800bff42ad042f5c8664dbb82d8e46d53d
yashrajvrma <vermayashraj2005@gmail.com>
2026-03-18 22:11:48 +0530

    feat: redirect user to dashboard from home on active session

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,17 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  // const session = await auth.api.getSession({
-  //   headers: await headers(),
-  // });
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
   // if (!session || !session.user) {
   //   redirect("/login");
   // }
+
+  if (session) {
+    redirect("/app");
+  }
 
   return (
     <div>
