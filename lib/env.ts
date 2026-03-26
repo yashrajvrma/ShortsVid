bbd53e5b6e9da0cf380a61421fc439c347a6b66f
yashrajvrma <vermayashraj2005@gmail.com>
2026-03-26 23:35:54 +0530

    feat: update env

--- lib/env.ts
+++ lib/env.ts
@@ -4,9 +4,13 @@ import { createEnv } from "@t3-oss/env-nextjs";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().min(1),
+
     BETTER_AUTH_SECRET: z.string().min(1),
+    BETTER_AUTH_URL: z.string().min(1),
+
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     GOOGLE_CLIENT_ID: z.string().min(1),
+
     OPENAI_API_KEY: z.string().min(1),
 
     // cloudflare api key
@@ -18,12 +22,25 @@ export const env = createEnv({
     // fish audio
     FISH_AUDIO_API_KEY: z.string().min(1),
 
+    // together ai
+    TOGETHER_AI_API_KEY: z.string().min(1),
+
+    // remotion
+    REMOTION_GCP_PRIVATE_KEY: z.string().min(1),
+    REMOTION_GCP_PROJECT_ID: z.string().min(1),
+    GCP_SERVE_URL: z.string().min(1),
+    GCP_SITE: z.string().min(1),
+
     // polar
     POLAR_SERVER_ENV: z.enum(["sandbox", "production"]).default("sandbox"),
+
     POLAR_BASIC_MONTHLY_PRODUCT_ID: z.string().min(1),
     POLAR_BASIC_YEARLY_PRODUCT_ID: z.string().min(1),
     POLAR_PRO_MONTHLY_PRODUCT_ID: z.string().min(1),
     POLAR_PRO_YEARLY_PRODUCT_ID: z.string().min(1),
+
+    POLAR_ACCESS_TOKEN: z.string().min(1),
+    POLAR_WEBHOOK_SECRET: z.string().min(1),
   },
   experimental__runtimeEnv: {},
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
