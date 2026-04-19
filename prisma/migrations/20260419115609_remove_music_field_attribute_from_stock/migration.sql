017ffb9850a4b3347568cbf44ecf3c7858679b8f
yashrajvrma <vermayashraj2005@gmail.com>
2026-04-19 17:45:55 +0530

    fix: remove music field from stock

---
+++ prisma/migrations/20260419115609_remove_music_field_attribute_from_stock/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [MUSIC] on the enum `StockType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "StockType_new" AS ENUM ('VOICE', 'BG_VIDEO', 'BG_MUSIC', 'AI_AVATAR');
+ALTER TABLE "stock" ALTER COLUMN "stockType" TYPE "StockType_new" USING ("stockType"::text::"StockType_new");
+ALTER TYPE "StockType" RENAME TO "StockType_old";
+ALTER TYPE "StockType_new" RENAME TO "StockType";
+DROP TYPE "public"."StockType_old";
+COMMIT;
